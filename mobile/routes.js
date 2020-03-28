cf9757a950609e742af2741b47453d2aebdf23aa
Frederico <fred@soujmv.com>
2020-03-28 04:34:47 -0300

    mobile application

---
+++ mobile/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Incidents from './src/pages/Incidents';
+import Detail from './src/pages/Detail';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Incidents" component={Incidents} />
+                <Stack.Screen name="Detail" component={Detail} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
